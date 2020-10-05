99b993397170ae55bfcd399e940d71473f5d4459
brunoluciano <bruno.lucian0544@gmail.com>
2020-10-05 00:37:14 -0300

    Iniciado estrutura do front e back end

---
+++ app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title> Minha Compra </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <layout-component></layout-component>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import LayoutComponent from "./components/MainLayout.vue";
+
+export default {
+  name: "LayoutDefault",
+
+  components: {
+    LayoutComponent,
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ app/src/quasar.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+import './styles/quasar.sass'
+import lang from 'quasar/lang/pt-br.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: { /* not needed if importStrategy is not 'manual' */ },
+  directives: { /* not needed if importStrategy is not 'manual' */ },
+  plugins: {
+  },
+  lang: lang
+ })
